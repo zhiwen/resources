8376fd19549e5e88d5e1f2fce9b129e6c93dca20
gitlab <zhiwen.mizw@alibaba-inc.com>
2014-11-16 23:48:39 +0800

    url+len

--- service/src/main/java/com/resources/dal/mapper/ResKVMapper.java
+++ service/src/main/java/com/resources/dal/mapper/ResKVMapper.java
@@ -1,7 +1,10 @@
 package com.resources.dal.mapper;
 
+import java.util.Map;
+
 import com.resources.dal.dataobject.ResKVDO;
 
 public interface ResKVMapper extends BaseMapper<ResKVDO, Long> {
 
+    public ResKVDO getDataByKeyWithType(Map<String, ? extends Object> param);
 }
