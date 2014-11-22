578ab9c42c9be39a0cf7c8c58eda3a2a9bb0871d
gitlab <zhiwen.mizw@alibaba-inc.com>
2014-11-23 03:54:19 +0800

    setup env

--- start-web.sh
+++ start-web.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
-mvn clean install -Dmaven.test.skip=true
+#mvn clean install -Dmaven.test.skip=true
 
-ln -s /Users/zhiwenmizw/Public/dev/workcontent/resources/deploy/deploy.web/target/deploy.web-1.0-SNAPSHOT.war /opt/tomcat7/deploy/web.war
+
+TOMCAT_HOME="/opt/tomcat7/"
+
+rm -rf ${TOMCAT_HOME}/webapps/web ${TOMCAT_HOME}/webapps/web.war
+
+ln -s /Users/zhiwenmizw/Public/dev/workcontent/resources/deploy/deploy.web/target/deploy.web-1.0-SNAPSHOT.war ${TOMCAT_HOME}/webapps/web.war
+
+CATALINA_OPTS="-server -Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=8899"
+
+sh ${TOMCAT_HOME}/bin/startup.sh
